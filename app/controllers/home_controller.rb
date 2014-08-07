418409acbacb9f051808eedc9977fa55af9ed61a
rails <rails@rails.com>
2014-08-07 23:00:37 +0530

    Refactored code to remove duplication

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -11,14 +11,12 @@ class HomeController < ApplicationController
   def land
     @properties = Property.where(:city => params[:city]).order("created_at asc")
     
-    p @properties
-    p "**********************************"
-    
-    
     if params[:search].present? and (!params[:search][:location].blank? )
+      # do something
     end
+    
     @json1 = "[{\"lat\":#{params[:lat]},\"lng\":#{params[:lon]}}]"
-    @json2 = Property.where(:city => params[:city]).to_gmaps4rails
+    @json2 = @properties.to_gmaps4rails
     @lat = params[:lat]
     @lon = params[:lon]
     @json = (@json1 + @json2).gsub("][",",")
