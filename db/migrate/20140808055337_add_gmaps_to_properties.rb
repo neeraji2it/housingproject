18b95dfb1cb17bb7da7627a8e1965dd19a3e45d0
rails <rails@rails.com>
2014-08-08 11:34:02 +0530

    small changes

--- db/migrate/20140808055337_add_gmaps_to_properties.rb
+++ db/migrate/20140808055337_add_gmaps_to_properties.rb
@@ -1,6 +1,6 @@
 class AddGmapsToProperties < ActiveRecord::Migration
   def change
-    add_column :properties, :gmaps, :boolean
+    change_column :properties, :gmaps, :boolean
     add_column :properties, :latitude, :float
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140807114348) do
+ActiveRecord::Schema.define(version: 20140808055337) do
 
   create_table "admins", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -93,7 +93,7 @@ ActiveRecord::Schema.define(version: 20140807114348) do
     t.datetime "image_updated_at"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "gmaps"
+    t.boolean  "gmaps"
     t.float    "latitude",              limit: 24
   end
 
