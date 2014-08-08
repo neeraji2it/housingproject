f4e333f87f38b45eb9ca990c4691b24e643cf25d
rails <rails@rails.com>
2014-08-08 12:34:31 +0530

    small changes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -9,19 +9,8 @@ class HomeController < ApplicationController
   end
   
   def land
-    @properties = Property.where(:city => params[:city]).order("created_at asc")
-    
-    if params[:search].present? and (!params[:search][:location].blank? )
-      # do something
-    end
-    
-    @json1 = "[{\"lat\":#{params[:lat]},\"lng\":#{params[:lon]}}]"
+    @properties = Property.where(:city => params[:city]).order(:price)
     @json2 = @properties.to_gmaps4rails
-    @lat = params[:lat]
-    @lon = params[:lon]
-    @json = (@json1 + @json2).gsub("][",",")
-    
-
     @city = params[:city]
   end
   
