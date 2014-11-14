2a6f858ebda0d91ae3eced4711528f3b630539b4
rails <rails@rails.com>
2014-11-14 12:42:20 +0530

    changed 1 admins

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,9 +2,8 @@
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
 # Examples:
-#
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 Admin.create(:email=>"dinemedia_admin@gmail.com", :password=>'password', :password_confirmation=>'password')
 Admin.create(:email=>"svrforms@gmail.com", :password=>'bangalore@123', :password_confirmation=>'bangalore@123')
-Admin.create(:email=>"smaranreddy123@gmail.com", :password=>'bhel@123', :password_confirmation=>'bhel@123')
+Admin.create(:email=>"smaranreddy123@gmail.com",:password=>'bhel@123',:password_confirmation=>'bhel@123')
