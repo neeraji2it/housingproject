92d2c65c27234376398e715f6218ee5a9fb97ddd
rails <rails@gmail.com>
2014-12-17 18:12:09 +0530

    updated images

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,6 @@
-set :domain, "107.155.113.112"
+set :domain, "107.155.87.187"
 set :rails_env, 'production'
 set :gitrepo, "git@github.com:neeraji2it/housingproject.git"
-set :deploydir, "/var/www/house.com/public_html"
-set :user, "shivraj"
+set :deploydir, "/var/www/svr.com/public_html" #, "/var/www/house.com/public_html"
+set :user, "admin"  #, "shivraj"
 set :use_sudo, false
