42eb08d04191dccf1586c72482fefd9ff97b45ab
rails <rails@gmail.com>
2014-12-17 18:43:18 +0530

    deleted images

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,4 +3,5 @@ set :rails_env, 'production'
 set :gitrepo, "git@github.com:neeraji2it/housingproject.git"
 set :deploydir, "/var/www/svr.com/public_html" #, "/var/www/house.com/public_html"
 set :user, "admin"  #, "shivraj"
+set :branch, "svrforms"
 set :use_sudo, false
