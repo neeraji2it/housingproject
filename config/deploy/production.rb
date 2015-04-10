01e14816d59c22390db81fe8f7c46ba53fbaacb7
rails <rails@rails.com>
2015-04-10 18:52:03 +0530

    ip address changed in deployment

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
-set :domain, "107.155.113.112"
+set :domain, "107.155.87.187"
 set :rails_env, 'production'
 set :gitrepo, "git@github.com:neeraji2it/housingproject.git"
 set :deploydir, "/var/www/house.com/public_html"
