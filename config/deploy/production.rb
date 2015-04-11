df75106f2842404f0a3673753fb7afd45fa3082b
rails <rails@rails.com>
2015-04-11 15:35:50 +0530

    testing for deployment

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,5 +2,5 @@ set :domain, "107.155.87.187"
 set :rails_env, 'production'
 set :gitrepo, "git@github.com:neeraji2it/housingproject.git"
 set :deploydir, "/var/www/house.com/public_html"
-set :branch, 'master'
-set :user, 'shivraj'
+set :user, "shivraj"
+set :use_sudo, false
