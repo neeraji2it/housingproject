4bbf3e1d4903e56996ea34a3b94694cd8cdb05df
rails <rails@rails.com>
2015-04-11 13:43:29 +0530

    deploy changes in production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,9 +2,6 @@ set :domain, "107.155.87.187"
 set :rails_env, 'production'
 set :gitrepo, "git@github.com:neeraji2it/housingproject.git"
 set :deploydir, "/var/www/house.com/public_html"
-set :user, "shivraj"
-set :use_sudo, false
-
-
-
-
+set :branch, 'master'
+set :user, 'shivraj'
+set :log_level, :debug
