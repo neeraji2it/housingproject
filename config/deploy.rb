cfba3bc6e5dcbd1642010bc33d80afc93419b3f0
rails <rails@rails.com>
2015-04-15 12:38:21 +0530

    city circles of images postion changes

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,6 +21,7 @@ set :keep_releases, 4
 desc "Symlinks database.yml, mailer.yml file from shared directory into the latest release"
 task :symlink_shared, :roles => [:app, :db] do
   run "ln -s #{shared_path}/config/database.yml #{latest_release}/config/database.yml"
+  run "ln -s #{shared_path}/cache #{latest_release}/tmp/cache"
   run "ln -s #{shared_path}/system #{latest_release}/system"
 #  run "ln -s #{shared_path}/public/system/attaches #{latest_release}/public/system/attaches"
 end
