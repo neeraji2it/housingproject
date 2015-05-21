0aa901b0b6ab6b2b2412b0464e9ee3c536ec67b1
rails <rails@rails.com>
2015-05-21 17:06:50 +0530

    changing text in contact

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,7 +1,7 @@
 class Property < ActiveRecord::Base
   has_attached_file :document
-  validates_attachment_content_type :document, :content_type => ["application/msword",'application/pdf']
- # validates_attachment_presence :document
+  validates_attachment_content_type :document, :content_type => ["application/msword","text/plain",'application/pdf'], :message => 'Only PDF, WORD or TEXT files are allowed. '
+  #validates_attachment_presence :document
   validates :location, :city, :description, :state,:city,:email, :presence => true
     has_many :images , :dependent => :destroy
   accepts_nested_attributes_for :images, :allow_destroy => true, :reject_if=>:all_blank
