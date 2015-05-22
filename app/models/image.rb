9898880f9f7289fad940ef4c91692d76973206d1
rails <rails@rails.com>
2015-05-22 11:01:43 +0530

    image validation and image display in gallery page

--- app/models/image.rb
+++ app/models/image.rb
@@ -2,5 +2,5 @@ class Image < ActiveRecord::Base
     belongs_to :property
   has_attached_file :image
   validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
- # validates_attachment_presence :image
+  validates_attachment_presence :image
 end
