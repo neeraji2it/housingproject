ae5d3f5a49b5f00bfe6a1e52054f9c9e2e65f6f7
rails <rails@rails.com>
2015-06-30 15:42:00 +0530

    add dotenv for managing environment variables

--- Gemfile
+++ Gemfile
@@ -38,6 +38,7 @@ gem 'gmaps4rails', '2.1.1'
 gem 'capistrano', '~> 3.4.0'
 gem 'capistrano-rails', '~> 1.1'
 gem 'capistrano-rvm'
+gem 'dotenv-rails'
 
 
 
--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 # config valid only for current version of Capistrano
 lock '3.4.0'
 
-set :application, 'Housing Project'
+set :application, 'Housing'
 set :repo_url, 'git@github.com:neeraji2it/housingproject.git'
 
 
