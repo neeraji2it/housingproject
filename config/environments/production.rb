c8bc883bd5ab64486ab34ae52c3a11533ef3752c
Ranjana Choudhary <rockingrails@gmail.com>
2015-07-14 15:21:09 +0530

    changed production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,6 +73,7 @@ Rails.application.configure do
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
   config.action_mailer.delivery_method = :smtp
+    config.action_mailer.default_url_options = { :host => 'http://svrfarms.com/' }
   ActionMailer::Base.smtp_settings = {
     :enable_starttls_auto => true,
     :address => 'smtp.gmail.com',
